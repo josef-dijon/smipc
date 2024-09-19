4728ffbf0733da2bf1cf5d8c83e54000f255be37
Josef de Joanelli <josef@pixelrift.io>
2024-09-19 13:26:31 +1000

    fix: remove benchmark workflow and do it via the build script

--- build.sh
+++ build.sh
@@ -7,5 +7,30 @@ else
 	cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE=Release
 fi
 
+if [[ $? -ne 0 ]]
+then
+	exit 1
+fi
+
 cmake --build build
+
+if [[ $? -ne 0 ]]
+then
+	exit 1
+fi
+
 ctest --test-dir build -V
+
+if [[ $? -ne 0 ]]
+then
+	exit 1
+fi
+
+jq --argjson results "$(cat build/libs/serdes/test/benchmarks/results.json )" '."benchmark-runs" += [$results]' test/serdes_benchmark_history.json > build/tmp_serdes_benchmark_history.json
+
+if [[ $? -ne 0 ]]
+then
+	exit 1
+fi
+
+mv build/tmp_serdes_benchmark_history.json test/serdes_benchmark_history.json
