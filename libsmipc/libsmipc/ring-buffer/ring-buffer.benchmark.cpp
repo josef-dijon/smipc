853137a3819a0bb9298e745459dd17819d72c181
Josef de Joanelli <josef@pixelrift.io>
2024-10-05 22:22:49 +1000

    test: add ring_buffer benchmarks

--- libsmipc/libsmipc/ring-buffer/ring-buffer.benchmark.cpp
+++ libsmipc/libsmipc/ring-buffer/ring-buffer.benchmark.cpp
@@ -1,17 +1,17 @@
 /* MIT License
- * 
+ *
  * Copyright (c) 2024 Josef de Joanelli (josef@pixelrift.io)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,14 +26,59 @@
 
 #include <benchmark/benchmark.h>
 
-static void BM_DeserialiseComplex(benchmark::State& state)
+static void BM_push_pop_1(benchmark::State& state)
 {
+	constexpr std::size_t kSize = 1024;
+	uint8_t buffer[kSize];
+
+	RingBufferRx<kSize> rx {buffer};
+	RingBufferTx<kSize> tx {buffer};
+
+	Packet packet {
+		std::vector<uint8_t> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+    };
+
 	for (auto _ : state)
 	{
-		;
+		tx.push(packet);
+		auto p1 = rx.pop();
+
+		benchmark::DoNotOptimize(p1);
 	}
 }
 
-BENCHMARK(BM_DeserialiseComplex);
+static void BM_push_pop_4(benchmark::State& state)
+{
+	constexpr std::size_t kSize = 1024;
+	uint8_t buffer[kSize];
+
+	RingBufferRx<kSize> rx {buffer};
+	RingBufferTx<kSize> tx {buffer};
+
+	Packet packet {
+		std::vector<uint8_t> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+    };
+
+	for (auto _ : state)
+	{
+		tx.push(packet);
+		tx.push(packet);
+		tx.push(packet);
+		tx.push(packet);
+
+		auto p1 = rx.pop();
+		auto p2 = rx.pop();
+		auto p3 = rx.pop();
+		auto p4 = rx.pop();
+
+		benchmark::DoNotOptimize(p1);
+		benchmark::DoNotOptimize(p2);
+		benchmark::DoNotOptimize(p3);
+		benchmark::DoNotOptimize(p4);
+	}
+}
+
+BENCHMARK(BM_push_pop_1);
+BENCHMARK(BM_push_pop_4);
 
 BENCHMARK_MAIN();
